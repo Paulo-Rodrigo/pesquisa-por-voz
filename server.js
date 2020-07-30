d0a60183b8886791a4e19117f029c0f2557e7f2b
Paulo-Rodrigo <p.rodrigo.martins@gmail.com>
2020-07-30 18:44:33 -0300

    Pesquisa

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res){
+
+    res.writeHead(200, { 'content-type': 'text.html'});
+
+    fs.createReadStream('index.html').pipe(res);
+});
+
+server.listen(3000);
